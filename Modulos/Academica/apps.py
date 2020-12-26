f78146fc339d7ac44ba378830a8ce666db45ee03
Eduardo <eduardorostran@yahoo.com>
2020-12-26 16:48:56 -0600

    modulos

---
+++ Modulos/Academica/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AcademicaConfig(AppConfig):
+    name = 'Academica'
--- mi_universisdad/settings.py
+++ mi_universisdad/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'es-ni'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'America/Managua'
 
