45ec51893dacd6f57433246390604c2353f503d9
Eduardo <eduardorostran@yahoo.com>
2020-12-29 20:55:19 -0600

    terminado

--- mi_universisdad/settings.py
+++ mi_universisdad/settings.py
@@ -120,7 +120,7 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
-#Enviar correo 
+#Enviar correo ...
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
